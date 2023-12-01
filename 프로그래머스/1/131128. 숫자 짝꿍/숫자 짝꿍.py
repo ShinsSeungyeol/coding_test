28ec620d253d48d19e76391553030aece27f4601
ShinsSeungyeol <85426685+ShinsSeungyeol@users.noreply.github.com>
2023-12-01 11:40:00 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,30 +1,13 @@
 def solution(X, Y):
     answer = ''
     
-    d_x = { str(i):0 for i in range(10)}
-    d_y = { str(i):0 for i in range(10)}
-    d_common = {}
-    for v in X:
-        d_x[v] += 1
-        
-    for v in Y:
-        d_y[v] += 1
-        
+    for i in range(9, -1, -1):
+        answer += str(i) * min(X.count(str(i)), Y.count(str(i))) 
     
-    for i in range(10):
-        if (d_x[str(i)] != 0 and d_y[str(i)] != 0):
-            n = min(d_x[str(i)], d_y[str(i)])
-            d_common[str(i)] = n
-            
-    if (d_common != {}):
-        for i in range(9, -1, -1):
-            if (str(i) in d_common):
-                if (str(i) == '0' and answer == ''):
-                    answer = '0'
-                else:
-                    answer += str(i) * d_common[str(i)]
-    else:
+    if answer == '':
         answer = '-1'
+    elif answer.count('0') == len(answer):
+        answer = '0'
         
     
     return answer
