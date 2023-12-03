af47192515e843a50ad56ee7798a30d017748808
ShinsSeungyeol <85426685+ShinsSeungyeol@users.noreply.github.com>
2023-12-03 21:55:24 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.27 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,15 @@
+import re
+def solution(new_id):
+    new_id = new_id.lower();
+    new_id = re.sub(r'[^a-z0-9\-_.]', '', new_id)
+    new_id = re.sub(r'\.{2,}', '.', new_id)
+    new_id = re.sub(r'^\.|\.$', '', new_id)
+    new_id = 'a' if len(new_id) == 0 else new_id
+
+    new_id = new_id[0:15]
+    new_id = re.sub(r'\.$', '', new_id)
+    
+    while len(new_id) < 3:
+        new_id += new_id[-1]
+
+    return new_id
