8cd362612ddf0c8749bccf2ed94c2265a2bce098
ShinsSeungyeol <85426685+ShinsSeungyeol@users.noreply.github.com>
2023-12-22 16:45:57 +0900

    [level 2] Title: 올바른 괄호, Time: 7.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = True
+    
+    stack = []
+    
+    for c in s: 
+        if c == '(':
+            stack.append(c)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return False
+            
+    if stack:
+        answer = False
+
+    return answer
